0835fb9710494b2979ea2c47e81c2f6a2d059e21
ssss <shalithajayamal96@gmail.com>
2020-01-30 23:20:06 +0530

    valiation routes updates

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -5,3 +5,13 @@ const customer = require("../controllers/customer.controller");
 router.get("/select", customer.view);
 router.post("/select", customer.select);
 module.exports = router;
+  
+
+const express = require("express");
+const router = express.Router();
+const customer = require("../controllers/customer.controller");
+const validateCust = require("../validation/cust.validation");
+
+router.get("/select", customer.view);
+router.post("/", validateCust, customer.create);
+module.exports = router;
