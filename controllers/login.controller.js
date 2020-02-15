9a53a6b6ef8587f6b17b99e132ec5ee40f53735b
yasindudehan <43140925+yasindudehan@users.noreply.github.com>
2020-02-15 23:41:34 +0530

    change

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,16 +1,14 @@
-const Rep = require('../models/rep.model');
-const Dist = require('../models/dist.model')
-const bcrypt =require("bcryptjs");
+const Rep = require("../models/rep.model");
+const Dist = require("../models/dist.model");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 /* 
-
 exports.signIn = (req, res) => {
   return User.findOne({
     Username: req.body.Username
   }).exec().then(user => {
     console.log(user);
-
    
-
     return user.isValidPassword(req.body.Password).then(isValid => {
       return res.status(200).json({
         message: "Login successful",
@@ -24,48 +22,68 @@ User.findOne({username}).then(user=>{
   if(!user){
       return res.status(404).json({incorrect: "Incorrect Username or password"});
   } */
-  exports.signIn = (req, res) => {
-    const username =req.body.username;
-    const password =req.body.password;
-    Rep.findOne({userName:username,status:"active"}).then(user=>{
-      if(!user){
-        Dist.findOne({userName:username,status:"active"}).then(dist=>{
-            if(!dist){
-                return res.status(404).json({incorrect: "Incorrect Username or password"});
-            }else{            
-            bcrypt.compare(password,dist.password).then(isMatch=>{
-                if(isMatch){
-                  return res.status(200).json({repordist:"1"});
-                }else{
-                    return res
-                    .status(400)
-                    .json({incorrect: "Incorrect Username or password"});
-                }
-            }).catch(err=>{
-          console.error("error found3!!!"+err);
-        });
-      }       
-    }).catch(err=>{
-      console.error("error found4!!!"+err);
-    })
-  } 
-      else{
-      
-      bcrypt.compare(password,user.password).then(isMatch=>{
-          if(isMatch){
-            return res.status(200).json({repordist:"0"});
-          }else{
+
+exports.signIn = (req, res) => {
+  const username = req.body.username;
+
+  const password = req.body.password;
+  Rep.findOne({ userName: username, status: "active" })
+    .then(user => {
+      if (!user) {
+        Dist.findOne({ userName: username, status: "active" })
+          .then(dist => {
+            if (!dist) {
               return res
-              .status(400)
-              .json({incorrect: "Incorrect Username or password"});
-          }
-      }).catch(err=>{
-        console.error("error found2!!!"+err);
-      });
-    }
-  }).catch(err=>{
-    console.error("error found1!!!"+err);
-  });
-  
-  
+                .status(404)
+                .json({ incorrect: "Incorrect Username or password" });
+            } else {
+              bcrypt
+                .compare(password, dist.password)
+                .then(isMatch => {
+                  if (isMatch) {
+                    return res.status(200).json({ repordist: "1" });
+                  } else {
+                    return res
+                      .status(400)
+                      .json({ incorrect: "Incorrect Username or password" });
+                  }
+                })
+                .catch(err => {
+                  console.error("error found3!!!" + err);
+                });
+            }
+          })
+          .catch(err => {
+            console.error("error found4!!!" + err);
+          });
+      } else {
+        bcrypt
+          .compare(password, user.password)
+          .then(isMatch => {
+            if (isMatch) {
+              return res.status(200).json({ repordist: "0" });
+            } else {
+              return res
+                .status(400)
+                .json({ incorrect: "Incorrect Username or password" });
+            }
+          })
+          .catch(err => {
+            console.error("error found2!!!" + err);
+          });
+      }
+    })
+    .catch(err => {
+      console.error("error found1!!!" + err);
+    });
+};
+exports.repInfo = (req, res) => {
+  const username = req.body.userName;
+  Rep.find({ userName: username }, { _id: 0, distributor: 1, area: 1 })
+    .then(rep => {
+      res.status(201).json(rep);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
 };
